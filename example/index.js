7a0b3b07d23a70684179b125e4c256ed19a195a4
Taylor Buckner <tbuckner@theappraisallane.com>
2019-05-14 15:14:41 -0500

    Need to add support for multiple entry files.

---
+++ example/index.js
@@ -0,0 +1,11 @@
+const LazyVinLib = require('../dist/lazy-vin-lib.umd');
+const SpongebobCase = require('../dist/spongebob-case.umd');
+const Simple = require('../dist/');
+const myLazyVin = new LazyVinLib()
+
+const iterations = 50;
+for (let i = 0; i < iterations; i++) {
+  const vin = myLazyVin.getRandomCleanVin();
+  const test = myLazyVin.fixCheckDigit(vin);
+  console.log({vin, test});
+}
